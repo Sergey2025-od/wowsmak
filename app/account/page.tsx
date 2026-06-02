373338931f29ed7d63a1f9a0e41178b4b8d82cca
Sergey2025-od <2017obzor2017@gmail.com>
2026-06-02 23:07:57 +0300

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,1 +1,8 @@
-
+export default function AccountPage() {
+	return (
+		<div>
+			<h1 className="text-2xl font-black">Кабінет</h1>
+			<p className="mt-2 text-white/70">Далі додамо авторизацію та привʼязку Telegram.</p>
+		</div>
+	)
+}
