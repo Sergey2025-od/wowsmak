aeba88475c48afc524d9501050d03fbdb416dc49
Sergey2025-od <2017obzor2017@gmail.com>
2026-06-02 22:57:05 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        candy: '#e6447a',
+        cream: '#fff7f0',
+        neonBlue: '#46d7ff',
+        neonPurple: '#9b5cff',
+      },
+    },
+  },
+  plugins: [],
+}
